6b3ea4e1494741d63230f7467500c18572ca2e4d
Mangesh1728 <mangeshsshinde2016@gmail.com>
2023-12-03 13:48:44 +0530

    first commit

--- app.py
+++ app.py
@@ -18,23 +18,52 @@ def predict_price(horsepower, curbweight, enginesize):
 
     return prediction[0]
 
+# Sample car names for the dropdown list
+car_names = [
+    "Toyota Camry", "Honda Accord", "Ford Mustang", "Chevrolet Malibu", "Tesla Model S",
+    "Nissan Altima", "BMW 3 Series", "Mercedes-Benz C-Class", "Audi A4", "Lexus ES",
+    "Volkswagen Passat", "Hyundai Sonata", "Subaru Outback", "Mazda6", "Kia Optima",
+    "Jeep Grand Cherokee", "Ford Explorer", "Toyota RAV4", "Honda CR-V", "Subaru Forester"
+]
+
 # Streamlit App
-st.title('Car Price Prediction App')
-st.markdown("<h2 style='text-align: center;'>Enter Car Features</h2>", unsafe_allow_html=True)
+st.set_page_config(
+    page_title="Car Price Prediction App",
+    page_icon="🚗",
+    layout="wide"
+)
 
-# Input form for user input (center-aligned)
-col1, col2, col3 = st.columns(3)
+# Sidebar with user input
+st.sidebar.title('Car Details')
+car_name = st.sidebar.selectbox('Select Car Name', car_names)
+horsepower = st.sidebar.slider('Horsepower', min_value=50, max_value=300, value=150)
+curbweight = st.sidebar.slider('Curb Weight', min_value=1500, max_value=5000, value=3000)
+enginesize = st.sidebar.slider('Engine Size', min_value=50, max_value=500, value=200)
 
-with col1:
-    horsepower = st.slider('Horsepower', min_value=50, max_value=300, value=150)
-
-with col2:
-    curbweight = st.slider('Curb Weight', min_value=1500, max_value=5000, value=3000)
-
-with col3:
-    enginesize = st.slider('Engine Size', min_value=50, max_value=500, value=200)
-
-# Predict button (center-aligned)
-if st.button('Predict Price'):
+# Predict button
+if st.sidebar.button('Predict Price'):
     prediction = predict_price(horsepower, curbweight, enginesize)
-    st.markdown(f"<h3 style='text-align: center;'>Predicted Price: ${prediction:.2f}</h3>", unsafe_allow_html=True)
+    st.success(f"Predicted Price for {car_name}: ${prediction:.2f}")
+
+# Main content area
+st.title('Car Price Prediction App')
+
+# Display car image or additional information if available
+# You can customize this section based on your specific requirements
+
+# Display additional information or insights about the car features
+st.subheader('Car Features Overview')
+st.markdown(f"**Car Name:** {car_name}")
+st.markdown(f"**Horsepower:** {horsepower} HP")
+st.markdown(f"**Curb Weight:** {curbweight} lbs")
+st.markdown(f"**Engine Size:** {enginesize} cubic inches")
+
+# You can add more sections or insights as needed
+
+# Display a disclaimer or additional information at the bottom
+st.sidebar.markdown(
+    """
+    **Disclaimer:** This app provides estimated car prices based on user input and a pre-trained machine learning model.
+    The predictions are for informational purposes only and may not reflect the actual market prices.
+    """
+)
